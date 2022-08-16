6076400fed04c4fe3599d8f7d8c37a8618edf010
Alex Dinsmoor <alex.dinsmoor@fishtownanalytics.com>
2022-08-16 07:10:15 +0000

    add bonus exercise solutions

---
+++ models/silver/apjuice/dim_customers.sql
@@ -0,0 +1,13 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+select 
+    store_id || "-" || cast(id as string) as unique_id
+    , id
+    , store_id
+    , name
+    , email
+from {{ source('apjuice', 'users') }} 
---
+++ tests/products_with_negative_amount.sql
@@ -0,0 +1,14 @@
+
+{{
+    config(
+        error_if = '>10'
+        , warn_if = '>0'
+    )
+}}
+
+select *
+from {{ ref('sales_items') }}
+where product_cost < 0
+
+
+
