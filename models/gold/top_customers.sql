f22a94e9cddccbc5afd38ecf2799f58386ee572b
Claire Tan <claire.tany@icloud.com>
2022-08-16 06:45:39 +0000

    complete changes

---
+++ models/gold/top_customers.sql
@@ -0,0 +1,15 @@
+select
+  s.store_id,
+  ss.unique_customer_id,
+  c.name,
+  sum(product_cost) total_spend
+from
+  {{ ref('sales_items') }} s
+  join {{ ref('sales') }} ss on s.sale_id = ss.id
+  join {{ ref('dim_customers') }}  c  on ss.unique_customer_id = c.unique_id
+where
+  ss.unique_customer_id is not null
+group by s.store_id, ss.unique_customer_id, c.name
+order by total_spend
+limit 100
+  
