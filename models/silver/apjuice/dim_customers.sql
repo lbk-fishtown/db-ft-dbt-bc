707b1bc034f3ce5a12f6aad63db501fcf946b4d4
Alex Moses <alex.moses@prospa.com>
2022-12-07 02:17:34 +0000

    DBT Labs Completed

---
+++ models/silver/apjuice/dim_customers.sql
@@ -0,0 +1,7 @@
+Select 
+    store_id || "-" || cast(id as string) as unique_id
+    , id
+    , store_id
+    , name
+    , email
+    from {{source ('apjuice', 'users')}}
---
+++ tests/ product_with_negative_amount.sql
@@ -0,0 +1,7 @@
+{{ config(
+    error_if = '>10'
+    , warn_if = '>0'
+)}}
+
+select * from  {{ref('sales_items')}}
+where product_cost < 0
